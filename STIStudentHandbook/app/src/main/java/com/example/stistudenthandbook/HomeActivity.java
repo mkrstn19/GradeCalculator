1cd78e134863557a362fec74150c1b26ac24abd8
mkrstn19 <mkrstn19@gmail.com>
2020-10-24 14:02:47 +0800

    Explicit and Implicit
    Explicit and Implicit go to Home

--- STIStudentHandbook/app/src/main/java/com/example/stistudenthandbook/HomeActivity.java
+++ STIStudentHandbook/app/src/main/java/com/example/stistudenthandbook/HomeActivity.java
@@ -3,28 +3,86 @@ package com.example.stistudenthandbook;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class HomeActivity extends AppCompatActivity {
 
     ImageButton calculatorImageButton;
+    ImageButton historyImageButton;
+    ImageButton MusicImageButton;
+    ImageButton stiWebsiteImageButton;
+    TextView emailTextView;
+
+
+    String emailAddress;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        Intent fromMain = getIntent();
+        Bundle bundle = fromMain.getExtras();
+
+        String emailAddress = (String) bundle.get("email");
+
+
+
         calculatorImageButton = findViewById(R.id.calculatorImageButton);
+        historyImageButton = findViewById(R.id.historyImageButton);
+        MusicImageButton = findViewById(R.id.btnSTIHymn);
+        stiWebsiteImageButton = findViewById(R.id.stiWebsiteImageButton);
+        emailTextView = findViewById(R.id.emailTextView);
+
+        emailTextView.setText(emailAddress);
+
         calculatorImageButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                Intent intentToGradeCalculator = new Intent( HomeActivity.this, GradeCalculator.class);
+                Intent intentToGradeCalculator = new Intent(HomeActivity.this, GradeCalculator.class);
                 startActivity(intentToGradeCalculator);
 
             }
         });
+
+
+        historyImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intentToHistory = new Intent(HomeActivity.this, HistoryActivity.class);
+                startActivity(intentToHistory);
+
+            }
+        });
+
+        MusicImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intentToMusic = new Intent(HomeActivity.this, STIHymnActivity.class);
+                startActivity(intentToMusic);
+
+            }
+        });
+
+        stiWebsiteImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String url = "https://www.sti.edu/";
+                Intent intentoSTIWebsite = new Intent(Intent.ACTION_VIEW);
+                intentoSTIWebsite.setData(Uri.parse(url));
+                startActivity(intentoSTIWebsite);
+
+            }
+        });
     }
+
 }
